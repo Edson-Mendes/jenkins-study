9197c7863e6e6f2802624aaa1f59888861844a61
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-04-12 14:47:02 -0300

    feat: add greeting endpoint

---
+++ src/main/java/com/example/jenkinsstudy/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.jenkinsstudy.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+  @GetMapping
+  public ResponseEntity<String> getGreeting() {
+    String greeting = "Hi how are you?";
+    return ResponseEntity.ok(greeting);
+  }
+
+}
