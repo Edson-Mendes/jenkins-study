93ca47059aaa4ecb54a851051e43c29dd1e68029
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-04-16 14:07:21 -0300

    feature: update greeting controller

--- src/main/java/com/example/jenkinsstudy/controller/GreetingController.java
+++ src/main/java/com/example/jenkinsstudy/controller/GreetingController.java
@@ -1,18 +1,17 @@
 package com.example.jenkinsstudy.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
   @GetMapping
-  public ResponseEntity<String> getGreeting() {
-    String greeting = "Hi how are you?";
-    return ResponseEntity.ok(greeting);
+  public ResponseEntity<String> getGreeting(@RequestParam(name = "name", required = false) String name) {
+    String greetingTemplate = "Hello %s!";
+    String value = name != null ? name : "World";
+    return ResponseEntity.ok(greetingTemplate.formatted(value));
   }
 
 }
