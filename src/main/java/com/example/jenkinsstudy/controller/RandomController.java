3d2cf9a3695f22f460eaba010d6686413541a5da
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-04-18 15:48:38 -0300

    feat: generate number for given origin and bound

--- src/main/java/com/example/jenkinsstudy/controller/RandomController.java
+++ src/main/java/com/example/jenkinsstudy/controller/RandomController.java
@@ -1,9 +1,8 @@
 package com.example.jenkinsstudy.controller;
 
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -11,12 +10,26 @@ import java.util.Random;
 @RequestMapping("/random")
 public class RandomController {
 
-  @GetMapping("/number")
-  public ResponseEntity<Integer> getRandomNumber() {
-    Random random = new Random();
-    int randomInteger = random.nextInt();
+  private static final Random random = new Random();
 
-    return ResponseEntity.ok(randomInteger);
+  @GetMapping("/number")
+  public ResponseEntity<Integer> getRandomNumber(
+      @RequestParam(value = "origin", required = false, defaultValue = "-2147483648") int origin,
+      @RequestParam(value = "bound", required = false, defaultValue = "2147483647") int bound) {
+    return ResponseEntity.ok(generateRandomNumber(origin, bound));
+  }
+
+  private int generateRandomNumber(int origin, int bound) {
+    return random.nextInt(origin, bound);
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<ProblemDetail> handleIllegalArgument(IllegalArgumentException exception) {
+    ProblemDetail problemDetail = ProblemDetail.forStatus(400);
+    problemDetail.setTitle("Illegal argument exception");
+    problemDetail.setDetail(exception.getMessage());
+
+    return ResponseEntity.badRequest().body(problemDetail);
   }
 
 }
